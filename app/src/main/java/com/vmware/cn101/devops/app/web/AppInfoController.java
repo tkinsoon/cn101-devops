9b06d33996285b5bf7b5a9b7eec06fae61a8f1d0
tkinsoon <tkinsoon@pivotal.io>
2020-07-03 15:59:44 +0800

    Added MediaType.APPLICATION_JSON_VALUE

--- app/src/main/java/com/vmware/cn101/devops/app/web/AppInfoController.java
+++ app/src/main/java/com/vmware/cn101/devops/app/web/AppInfoController.java
@@ -6,6 +6,7 @@ import io.prometheus.client.Counter;
 import io.prometheus.client.Summary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/rest/app")
+@RequestMapping(value = "/rest/app", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AppInfoController implements Controller {
 
     private final Logger logger = LoggerFactory.getLogger(AppInfoController.class);
